2ec01e5302a9eda23a2cea571a9561184fcb8f66
T-MONIKA <monikat.23csd@kongu.edu>
2026-07-29 11:34:35 +0530

    Time: 320 ms (5.74%), Space: 47.5 MB (22.81%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,19 +1,23 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int total = 0;
-        for (int num : nums) {
-            total += num;
-        }
-        
-        int leftTotal = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int rightTotal = total - leftTotal - nums[i];
-            if (rightTotal == leftTotal) {
-                return i;
+        int n=nums.length;
+        int left;
+        int right;
+        int pivot;
+        for(int i=0;i<n;i++){
+            pivot=i;
+            left=0;
+            right=0;
+            for(int j=0;j<pivot;j++){
+                left=left+nums[j];
             }
-            leftTotal += nums[i];
+            for(int j=pivot+1;j<n;j++){
+                right=right+nums[j];
+            }
+            if(right==left){
+                return pivot;
+            }
         }
-
-        return -1;        
+        return -1;
     }
 }
