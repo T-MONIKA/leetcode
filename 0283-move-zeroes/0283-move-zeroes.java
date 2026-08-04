3b9520c92348219882c434b904537ad8cdd1a47a
T-MONIKA <monikat.23csd@kongu.edu>
2026-08-04 09:47:27 +0530

    Time: 2 ms (92.13%), Space: 47.3 MB (97.99%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int m=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[m]=nums[i];
+                m++;
+            }
+        }
+        while(m<nums.length) {
+            nums[m]=0;
+            m++;
+        }
+    }
+}
