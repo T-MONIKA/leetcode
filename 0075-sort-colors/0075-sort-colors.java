587072f63b0de3383ffbe816dc8dec3af990861e
T-MONIKA <monikat.23csd@kongu.edu>
2026-09-24 15:11:09 +0530

    Time: 1 ms (17.53%), Space: 43.6 MB (21.59%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,20 +1,14 @@
 class Solution {
-  public void sortColors(int[] nums) {
-    int l = 0;
-    int r = nums.length - 1;
-
-    for (int i = 0; i <= r;)
-      if (nums[i] == 0)
-        swap(nums, i++, l++);
-      else if (nums[i] == 1)
-        ++i;
-      else
-        swap(nums, i, r--);
-  }
-
-  private void swap(int[] nums, int i, int j) {
-    final int temp = nums[i];
-    nums[i] = nums[j];
-    nums[j] = temp;
-  }
+    public void sortColors(int[] nums) {
+        int temp;
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]>nums[j]){
+                    temp=nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=temp;
+                }
+            }
+        }
+    }
 }
